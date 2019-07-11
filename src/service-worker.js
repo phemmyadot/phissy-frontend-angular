c5cefb992d981005996d4e57f7ec835d5811a9f8
Badojutelegan <badojutelegan@eminenttechnology.com>
2019-07-11 12:25:20 +0100

    service worker

---
+++ src/service-worker.js
@@ -0,0 +1,69 @@
+const FILES_TO_CACHE = [
+    '/offline.html',
+  ];
+  var CACHE_NAME = "cache-v1";
+  self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+  }); 
+  self.addEventListener("fetch", (e) => {
+      e.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+              return caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    return cache.match('offline.html');
+                  });
+            })
+      );
+  });
+  
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+  
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  };
+  
+  workbox.routing.registerRoute(
+      /\.js$/,
+      new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+      /\.ts$/,
+      new workbox.strategies.NetworkFirst()
+    );
+    workbox.routing.registerRoute(
+      // Cache CSS files.
+      /\.css$/,
+      // Use cache but update in the background.
+      new workbox.strategies.StaleWhileRevalidate({
+        // Use a custom cache name.
+        cacheName: 'css-cache',
+      })
+    );
+    
+    workbox.routing.registerRoute(
+      // Cache image files.
+      /\.(?:png|jpg|jpeg|svg|gif)$/,
+      // Use the cache if it's available.
+      new workbox.strategies.CacheFirst({
+        // Use a custom cache name.
+        cacheName: 'image-cache',
+        plugins: [
+          new workbox.expiration.Plugin({
+            // Cache only 20 images.
+            maxEntries: 20,
+            // Cache for a maximum of a week.
+            maxAgeSeconds: 7 * 24 * 60 * 60,
+          })
+        ],
+      })
+    );
+  
+  
